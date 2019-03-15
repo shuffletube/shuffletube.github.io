f208287aba5a4ffef4eedd02946a5120c2c5b4bb
shuffletube <48561520+shuffletube@users.noreply.github.com>
2019-03-14 23:03:05 -0600

    Update googleauth.js

--- googleauth.js
+++ googleauth.js
@@ -74,26 +74,26 @@ var GoogleAuth;
     setSigninStatus();
   }
 
-  function start() {
-  // 2. Initialize the JavaScript client library.
-  gapi.client.init({
-    'apiKey': 'AIzaSyAQTQGAOoPucSFjT2KKZkzd2Xs9s1W-Aq8',
-    // Your API key will be automatically added to the Discovery Document URLs.
-    'discoveryDocs': ['https://people.googleapis.com/$discovery/rest'],
-    // clientId and scope are optional if auth is not required.
-    'clientId': '964128075521-nm4satik39m8g82hucr9a7lbadici98e.apps.googleusercontent.com',
-    'scope': 'profile',
-  }).then(function() {
-    // 3. Initialize and make the API request.
-    return gapi.client.people.people.get({
-      'resourceName': 'people/me',
-      'requestMask.includeField': 'person.names'
-    });
-  }).then(function(response) {
-    console.log(response.result);
-  }, function(reason) {
-    console.log('Error: ' + reason.result.error.message);
-  });
-};
-// 1. Load the JavaScript client library.
-gapi.load('client', start);
+//   function start() {
+//   // 2. Initialize the JavaScript client library.
+//   gapi.client.init({
+//     'apiKey': 'AIzaSyAQTQGAOoPucSFjT2KKZkzd2Xs9s1W-Aq8',
+//     // Your API key will be automatically added to the Discovery Document URLs.
+//     'discoveryDocs': ['https://people.googleapis.com/$discovery/rest'],
+//     // clientId and scope are optional if auth is not required.
+//     'clientId': '964128075521-nm4satik39m8g82hucr9a7lbadici98e.apps.googleusercontent.com',
+//     'scope': 'profile',
+//   }).then(function() {
+//     // 3. Initialize and make the API request.
+//     return gapi.client.people.people.get({
+//       'resourceName': 'people/me',
+//       'requestMask.includeField': 'person.names'
+//     });
+//   }).then(function(response) {
+//     console.log(response.result);
+//   }, function(reason) {
+//     console.log('Error: ' + reason.result.error.message);
+//   });
+// };
+// // 1. Load the JavaScript client library.
+// gapi.load('client', start);
